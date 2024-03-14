7ceb4809ef306e1ee81c784ae7903c4fd81061a3
Terminalblocked <mr.pro.simon@mail.ru>
2024-03-15 00:15:34 +0400

    update 1.1

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,12 +1,12 @@
-import jwt from 'jsonwebtoken';
 import { ApiError } from '../exeptions/apiError.js';
+import * as tokenService from '../services/tokenService.js';
 
 export default (req, res, next) => {
     const token = (req.headers.authorization || '').replace('Bearer ', '');
 
     if (token) {
         try {
-            const decodedToken = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            const decodedToken = tokenService.validateAccessToken(token);
             req.userId = decodedToken.id;
         } catch (error) {
             throw ApiError.UnauthorizedExeption();
